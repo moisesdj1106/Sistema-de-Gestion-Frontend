ffe445eefc8e05b3dbe67226f82a7d37eb03bad5
moisesdj1106 <moises.becerra2004@gmail.com>
2025-12-03 23:28:50 -0400

    coma

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -6,7 +6,7 @@ import {
 import bg from 'src/assets/images/carro.jpg';
 
 /*const API = 'http://localhost:4000';*/
-const API = 'https://sistema-de-gestion-backend.onrender.com'
+const API = 'https://sistema-de-gestion-backend.onrender.com';
 
 const Formulario = () => {
   const [step, setStep] = useState(1);

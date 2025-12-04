98d8126e538ff27bd4f45ee922253c96fce8ee73
moisesdj1106 <moises.becerra2004@gmail.com>
2025-12-03 23:31:21 -0400

    cambio

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -5,7 +5,7 @@ import {
 } from '@coreui/react';
 import bg from 'src/assets/images/carro.jpg';
 
-/*const API = 'http://localhost:4000';*/
+
 const API = 'https://sistema-de-gestion-backend.onrender.com';
 
 const Formulario = () => {
